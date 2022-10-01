9fce78ac8d2e7321213165012fc100126ef654f0
pkos <pkos@op.pl>
2022-10-01 16:12:55 +0200

    Adding function minMaxSum

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use max" #-}
 module Main where
+import Data.Ord (Ord)
 
 toIntsFromString :: String -> [Int]
 toIntsFromString s = let ws = words s in map read ws
@@ -8,8 +11,16 @@ maxMin (x : xs) = Just $ foldr
     (\x (max, min) -> (if x > max then x else max, if x < min then x else min)) 
     (x, x) 
     xs
-
 maxMin _ = Nothing
+
+minMaxSum :: Ord a => Num a => [a] -> Maybe (a, a, a)
+minMaxSum (x : xs) = Just $ foldr
+    (\x (min, max, sum) -> (if x < min then x else min, if x > max then x else max, sum + x))
+    (x, x, x)
+    xs
+minMaxSum _ = Nothing 
+
+
 
 
 main :: IO ()
@@ -21,7 +32,9 @@ main = do
     let
         valuesInt = toIntsFromString numbers
         stats = maxMin valuesInt
+        stats2 = minMaxSum valuesInt
 
     putStrLn numbers
     print valuesInt
     print stats
+    print stats2
